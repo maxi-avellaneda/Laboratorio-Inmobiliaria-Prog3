1b2496db9e1f97f0689f4e8f2b248ca545311588
cejasmiguel16 <mbcrfull@gmail.com>
2020-09-14 15:02:20 -0300

    Aplicacion de bootstrap a formulario

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
 
 function formularioCasa()   {
-    let formulario_principal = document.getElementById("form_general");
-    let boton_casa = document.getElementById("opc_casa").value;
-    let formulario_casa = document.getElementById("opciones_casa");
-    let formulario_departamento = document.getElementById("opciones_dpto");
-    let formulario_habitacion = document.getElementById("opciones_hab");
+    let formulario_principal = document.getElementById("form-principal");
+    let boton_casa = document.getElementById("opc-casa").value;
+    let formulario_casa = document.getElementById("opciones-casa");
+    let formulario_departamento = document.getElementById("opciones-dpto");
+    let formulario_habitacion = document.getElementById("opciones-hab");
     if(boton_casa == 1){
         formulario_principal.style.display = "block";
         formulario_casa.style.display = "block";
@@ -18,11 +18,11 @@ function formularioCasa()   {
 }
 
 function formularioDpto()   {
-    let boton_casa = document.getElementById("opc_dpto").value;
-    let formulario_principal = document.getElementById("form_general");;
-    let formulario_departamento = document.getElementById("opciones_dpto");
-    let formulario_habitacion = document.getElementById("opciones_hab");
-    let formulario_casa = document.getElementById("opciones_casa");
+    let boton_casa = document.getElementById("opc-dpto").value;
+    let formulario_principal = document.getElementById("form-principal");;
+    let formulario_departamento = document.getElementById("opciones-dpto");
+    let formulario_habitacion = document.getElementById("opciones-hab");
+    let formulario_casa = document.getElementById("opciones-casa");
     if(boton_casa == 2){
         formulario_principal.style.display = "block";
         formulario_departamento.style.display = "block";
@@ -35,11 +35,11 @@ function formularioDpto()   {
 }
 
 function formularioHab()    {
-    let boton_casa = document.getElementById("opc_hab").value;
-    let formulario_principal = document.getElementById("form_general");;
-    let formulario_habitacion = document.getElementById("opciones_hab");
-    let formulario_departamento = document.getElementById("opciones_dpto");
-    let formulario_casa = document.getElementById("opciones_casa");
+    let boton_casa = document.getElementById("opc-hab").value;
+    let formulario_principal = document.getElementById("form-principal");;
+    let formulario_habitacion = document.getElementById("opciones-hab");
+    let formulario_departamento = document.getElementById("opciones-dpto");
+    let formulario_casa = document.getElementById("opciones-casa");
     if(boton_casa == 3){
         formulario_principal.style.display = "block";
         formulario_habitacion.style.display = "block";
